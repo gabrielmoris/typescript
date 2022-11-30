f55d51655e394c51505cfa7e8c9fc7d1c43bd71f
gabriel <gabrielcmoris@gmail.com>
2022-11-30 14:35:07 +0100

    merge types, optional and readonly types

--- basic/objects.ts
+++ basic/objects.ts
@@ -20,3 +20,54 @@ function createCourse(): { name: string; price: number } {
 const newUser = { name: "gabriel", isPaid: false, email: "gabriel@gmail.com" };
 createUser(newUser);
 createUser({ name: "gabriel", isPaid: false, email: "gabriel@gmail.com" });
+
+// We can create a type alias
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function createrOtherUser(user: User) {}
+
+//The alias can be used as the normal types
+
+function createrOtherMoreUser(): User {
+  return { name: "", email: "", isActive: true };
+}
+
+//We can assign readonly and optional types
+
+type UserDb = {
+  readonly _id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+  creditCardDetails?: number;
+};
+
+let newUserDb: UserDb = {
+  _id: "12344",
+  name: "Hola",
+  email: "h@hola.col",
+  isActive: false,
+};
+
+//I can change the name
+newUserDb.name = "Saludos";
+//But not the ID
+newUserDb._id = "not allowed to change";
+
+//It is possible to merge types
+type cardNumber = {
+  cardNumber: string;
+};
+
+type cardDate = {
+  cardDate: string;
+};
+
+type cardDetails = cardNumber &
+  cardDate & {
+    cvv: number;
+  };
