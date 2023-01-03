6db6ea6b779f4b161768318d6c198e6028ff2cf4
gabriel <gabrielcmoris@gmail.com>
2023-01-03 11:50:53 +0100

    private

--- project/src/index.ts
+++ project/src/index.ts
@@ -4,15 +4,18 @@ class User {
   email: string;
   name: string;
   readonly city: string = "Coruña";
+  // User ID is only accesible inside of the class
+  private userId: number = 0;
   constructor(email: string, name: string) {
     this.email = email;
     this.name = name;
   }
 }
+
 const gabriel = new User("gabrielcmoris@gmail.com", "gabriel");
 
 const body = document.body;
-const htmlText = `Hello ${gabriel.name} your email is ${gabriel.email}`;
+const htmlText = `Hello ${gabriel.name} your email is ${gabriel.email} and you live in ${gabriel.city}.`;
 const htmlElement = document.createElement("h1");
 htmlElement.innerText = htmlText;
 body.appendChild(htmlElement);
